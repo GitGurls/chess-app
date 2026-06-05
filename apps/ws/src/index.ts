8e6ce1f753f446e10e5eb1ee97ab0c6db3f7c0c9
Soni <guptasoni30461@gmail.com>
2026-06-05 17:55:59 +0530

    Refactor WebSocket server to use environment port

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,10 +1,31 @@
+// import { WebSocketServer } from "ws";
+// import { GameManager } from "./GameManager";
+
+// const wss = new WebSocketServer({ port: 8080 });
+// const gameManager = new GameManager();
+
+// console.log("WebSocket server was started on port 8080");
+
+// wss.on("connection", (socket) => {
+//   console.log("Naya player was connected!");
+//   gameManager.addPlayer(socket);
+// });
+
+
+
+
 import { WebSocketServer } from "ws";
 import { GameManager } from "./GameManager";
 
-const wss = new WebSocketServer({ port: 8080 });
+const PORT = Number(process.env.PORT) || 8080;
+
+const wss = new WebSocketServer({
+  port: PORT,
+});
+
 const gameManager = new GameManager();
 
-console.log("WebSocket server was started on port 8080");
+console.log(`WebSocket server started on ${PORT}`);
 
 wss.on("connection", (socket) => {
   console.log("Naya player was connected!");
